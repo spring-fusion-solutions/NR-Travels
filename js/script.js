adeb6ee3251aa3e3e02b42cd61bf9ff2afb511be
Abdul_Malik <malik.info28@gmail.com>
2025-02-02 16:14:24 +0530

    added configurations and markup

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
