d3cba3daac44554d00dd40a25b6edcea27e22180
Abdul_Malik <malik.info28@gmail.com>
2025-02-02 18:03:22 +0530

    added navbar

--- js/script.js
+++ js/script.js
@@ -14,3 +14,32 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+// nav togler
+
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+btn.addEventListener('click', navToggle);
+
+function navToggle() {
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+}
+
+// Animation JS
+const elements = document.querySelectorAll('.scroll-right-side-animation');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate-once');
+            observer.unobserve(entry.target); // Stop observing once animated
+        }
+    });
+}, {
+    threshold: 0.4 // Trigger when 40% of the element is in view
+});
+
+elements.forEach(el => observer.observe(el));
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
+      screens: {
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1200px',
+        'xxl': '1400px',
+      },
       fontFamily: {
         sans: ['"Inter"', 'serif'],
         play: ['"Playfair"', 'serif']
